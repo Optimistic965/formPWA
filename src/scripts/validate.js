30a36b4487120730fb0bd009e7238a0c42b4262c
Ifeoluwa Olamiji <onabajoif@yahoo.com>
2023-12-24 22:12:16 +0100

    input field validation done

--- src/scripts/validate.js
+++ src/scripts/validate.js
@@ -3,15 +3,19 @@ const name = document.querySelector('input#name')
 const email = document.querySelector('input#email')
 const phNum = document.querySelector('input#number')
 
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const phoneNumberRegex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
 
 function displayErrMsg (currentEle) {
     const errEle = currentEle.previousElementSibling.querySelector('p.error_msg')
     errEle.style.display = 'block'
 }
+
 function removeErrMsg(currentEle) {
     const errEle = currentEle.previousElementSibling.querySelector('p.error_msg')
     errEle.style.display = 'none'
 }
+
 function valiate (activeSection) {
     // Validate all input field before moving on to the next
     switch(activeSection) {
@@ -20,10 +24,10 @@ function valiate (activeSection) {
             if (name.value.length > 3) {
                 removeErrMsg(name)
                 // then check for email
-                if (email.value !== '') {
+                if (emailRegex.test(email.value)) {
                     removeErrMsg(email)
                     // then check for number
-                    if (phNum.value.length > 5) {
+                    if (phoneNumberRegex.test(phNum.value)) {
                         removeErrMsg(phNum)
                         return true
                     } else {
